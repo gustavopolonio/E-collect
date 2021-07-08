62a5a3760310c287cca2587af45870cd82a559d2
Gustavo Polonio <gustavopolonio1@gmail.com>
2021-07-08 14:46:05 -0300

    page-create-point-forms

---
+++ scripts/create-point.js
@@ -0,0 +1,54 @@
+const stateSelectField = document.querySelector("select[name=uf]");
+const citySelectField = document.querySelector("select[name=city]");
+const stateInputField = document.querySelector("input[name=state]");
+const itemsInputField = document.querySelector("input[name=items]")
+
+async function populateUFs() {
+  await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+    .then(response => response.json())
+    .then(states => {
+      for (let state of states) {
+        stateSelectField.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      }
+    })
+}
+populateUFs()
+
+
+stateSelectField.addEventListener("change", async (event) => {
+  citySelectField.disabled = true;
+
+  const indexOfSelectedState = event.target.selectedIndex;
+  const selectedState = event.target.options[indexOfSelectedState].text;
+  stateInputField.value = selectedState;
+
+  const cityId = (event.target.value);
+  citySelectField.innerHTML = '<option value="">Choose your city</option>';
+
+  await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${cityId}/municipios`)
+    .then(response => response.json())
+    .then(cities => {
+      for (city of cities) {
+        citySelectField.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+      }
+
+      citySelectField.disabled = false;
+    })
+})
+
+let selectedItems = [];
+
+function testando(event) {
+  const buttonClicked = event.currentTarget;
+  buttonClicked.classList.toggle("selected");
+
+  const IDbuttonClicked = buttonClicked.dataset.id;
+  const indexOfIdButton = selectedItems.indexOf(IDbuttonClicked);
+
+  indexOfIdButton !== -1 
+  ? selectedItems.splice(indexOfIdButton, 1)
+  : selectedItems.push(IDbuttonClicked);
+
+  itemsInputField.value = selectedItems;
+}
+
